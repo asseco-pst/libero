21de73503217431443cd0183d1f3b35d16b128d4
Jose Coelho <jose.coelho@exictos.com>
2018-09-19 15:18:34 +0100

    Update playground

--- src/main/groovy/com/exictos/devops/Playground.groovy
+++ src/main/groovy/com/exictos/devops/Playground.groovy
@@ -1,12 +1,15 @@
 package com.exictos.devops
 
+import com.exictos.devops.cli.Libero
 import com.exictos.devops.helpers.LiberoHelper
 
 class Playground {
 
     static void main(String[] args){
 
-        println LiberoHelper.standardizeName("C:/BackOfficeWS.ear","backoffice","1.2.3")
+        String[] arg = ["-container","websphere","-host","***REMOVED***","-port","8888","-username","jenkins","-password","ieapdjnw70","-wsadmin","C:/IBM/WebSphere/AppServer/bin/wsadmin.bat","-appLocation","C:/Program Files (x86)/Jenkins/workspace/MW Modules (Sandbox)/ant/DistributionEAR/BackOfficeWS/2018-09-19_10-35-19/BackOfficeWSEAR.ear","-appName","BackofficeWS","-install","-start"]
+
+        Main.main(arg)
 
     }
 
