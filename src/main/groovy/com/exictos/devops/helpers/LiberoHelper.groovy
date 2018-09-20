d470e66b2c5864a4c24830f97b319b3563934bc8
Jose Coelho <jose.coelho@exictos.com>
2018-09-20 15:53:36 +0100

    Update Name validation regex

--- src/main/groovy/com/exictos/devops/helpers/LiberoHelper.groovy
+++ src/main/groovy/com/exictos/devops/helpers/LiberoHelper.groovy
@@ -19,7 +19,7 @@ class LiberoHelper {
     protected static final Logger log = LiberoLogger.getLogger()
 
     private static final String DATE_FORMAT = "yyyy-MM-dd_HH-mm-ss"
-    private static final String NAME_VALIDATOR_REGEX = "^([aA-zZ\\-]+)((_v)(?:[\\dx]{1,3}\\.){0,3}[\\dx]{1,3})?(___)"
+    private static final String NAME_VALIDATOR_REGEX = "^([aA-zZ0-9\\-]+)((_v)(?:[\\dx]{1,3}\\.){0,3}[\\dx]{1,3})?(___)"
     private static final String VERSION_VALIDATOR_REGEX = "^(?:[\\dx]{1,3}\\.){0,3}[\\dx]{1,3}\$"
 
     /**
