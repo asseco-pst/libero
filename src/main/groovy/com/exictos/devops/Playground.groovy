5396cc3ae94d0def004fb8701230fe480f7f504f
Jose Coelho <jose.coelho@exictos.com>
2018-10-11 12:05:14 +0100

    Add unit tests

--- src/main/groovy/com/exictos/devops/Playground.groovy
+++ src/main/groovy/com/exictos/devops/Playground.groovy
@@ -1,22 +1,8 @@
 package com.exictos.devops
 
-import com.exictos.devops.helpers.LiberoHelper
-
-import java.sql.Timestamp
-
 class Playground {
     
     static void main(String []args){
-
-        Timestamp now = new Timestamp(118,9,11,10,46,0,0)
-        String time = now.format(LiberoHelper.DATE_FORMAT)
-        String version = "1.2.3"
-        String appName = "BackOfficeWS"
-
-        String standardizedName = "${appName}___v${version}___${time}.ear"
-
-        String actual = LiberoHelper.extractTimestamp(standardizedName)
-        println actual
 
     }
 }
